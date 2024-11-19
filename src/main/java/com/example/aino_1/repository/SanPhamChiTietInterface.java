07afed191423f86c55f8db6d20a5416d5be893b7
Tsan333 <149345264+Tsan333@users.noreply.github.com>
2024-11-20 01:23:35 +0700

    sửa chức năng tìm kiếm và lọc

--- src/main/java/com/example/aino_1/repository/SanPhamChiTietInterface.java
+++ src/main/java/com/example/aino_1/repository/SanPhamChiTietInterface.java
@@ -70,167 +70,180 @@ public interface SanPhamChiTietInterface extends JpaRepository<SanPhamChiTiet, I
     List<String> findImagePathsByProductDetailId(@Param("id") Integer id);
 
 
+    @Query(
+            """
+                    SELECT DISTINCT new com.example.aino_1.dto.SanPhamChiTietDto(
+                    spct.id, spct.soLuong, sp.tenSanPham, cl.tenChatLieu, sp.gioiThieu, 
+                    ram.dungLuong, olt.dungLuong, mh.doPhanGiai, ktlt.kichThuoc, 
+                    mh.tamNen, mh.tanSoQuet, cpu.soNhan, gpu.kienTrucCongNghe, 
+                    cpu.ten, spct.maSpct, 
+                    spct.hinhAnhMinhHoa,
+                    spct.donGia, sp.id, gpu.ten, sp.trongLuong
+                    ) 
+                    FROM SanPhamChiTiet spct 
+                    JOIN SanPham sp ON spct.sanPham.id = sp.id 
+                    INNER JOIN ChatLieu cl ON cl.id = sp.chatLieu.id 
+                    INNER JOIN LoaiSanPham lsp ON lsp.id = sp.loaiSanPham.id 
+                    INNER JOIN KichThuocLapTop ktlt ON ktlt.id = sp.kichThuocLaptop.id 
+                    INNER JOIN NguonNhap nn ON nn.id = sp.nguonNhap.id 
+                    INNER JOIN ManHinh mh ON mh.id = spct.manHinh.id 
+                    INNER JOIN Ram ram ON ram.id = spct.ram.id 
+                    INNER JOIN OLuuTru olt ON olt.id = spct.oLuuTru.id 
+                    INNER JOIN Cpu cpu ON cpu.id = spct.cpu.id 
+                    INNER JOIN Gpu gpu ON gpu.id = spct.gpu.id 
+                    WHERE sp.tenSanPham LIKE %:tuKhoaTimKiem%
+                    OR sp.gioiThieu LIKE %:tuKhoaTimKiem%
+                    OR CAST(ram.dungLuong AS string) LIKE %:tuKhoaTimKiem%
+                    OR CAST(olt.dungLuong AS string) LIKE %:tuKhoaTimKiem%
+                    OR mh.doPhanGiai LIKE %:tuKhoaTimKiem%
+                    OR CAST(ktlt.kichThuoc AS string) LIKE %:tuKhoaTimKiem%
+                    OR mh.tamNen LIKE %:tuKhoaTimKiem%
+                    OR CAST(mh.tanSoQuet AS string) LIKE %:tuKhoaTimKiem%
+                    OR CAST(cpu.soNhan AS string) LIKE %:tuKhoaTimKiem%
+                    OR spct.maSpct LIKE %:tuKhoaTimKiem%
+                    OR cpu.ten LIKE %:tuKhoaTimKiem%
+                    """)
+    List<SanPhamChiTietDto> timSanPhamTheoTuKhoa(@Param("tuKhoaTimKiem") String tuKhoaTimKiem);
+
+    @Query(
+            "SELECT DISTINCT new com.example.aino_1.dto.SanPhamChiTietDto(" +
+                    "spct.id, spct.soLuong, sp.tenSanPham, cl.tenChatLieu, sp.gioiThieu, " +
+                    "ram.dungLuong, olt.dungLuong, mh.doPhanGiai, ktlt.kichThuoc, " +
+                    "mh.tamNen, mh.tanSoQuet, cpu.soNhan, gpu.kienTrucCongNghe, " +
+                    "cpu.ten, spct.maSpct, " +
+                    "spct.hinhAnhMinhHoa, " +
+                    "spct.donGia, sp.id, gpu.ten, sp.trongLuong" +
+                    ") " +
+                    "FROM SanPhamChiTiet spct " +
+                    "JOIN SanPham sp ON spct.sanPham.id = sp.id " +
+                    "INNER JOIN ChatLieu cl ON cl.id = sp.chatLieu.id " +
+                    "INNER JOIN LoaiSanPham lsp ON lsp.id = sp.loaiSanPham.id " +
+                    "INNER JOIN KichThuocLapTop ktlt ON ktlt.id = sp.kichThuocLaptop.id " +
+                    "INNER JOIN NguonNhap nn ON nn.id = sp.nguonNhap.id " +
+                    "INNER JOIN ManHinh mh ON mh.id = spct.manHinh.id " +
+                    "INNER JOIN Ram ram ON ram.id = spct.ram.id " +
+                    "INNER JOIN OLuuTru olt ON olt.id = spct.oLuuTru.id " +
+                    "INNER JOIN Cpu cpu ON cpu.id = spct.cpu.id " +
+                    "INNER JOIN Gpu gpu ON gpu.id = spct.gpu.id " +
+                    "WHERE spct.donGia between :minPrice and :maxPrice "
+    )
+    List<SanPhamChiTietDto> locTheoGia(@Param("minPrice") Double minPrice, @Param("maxPrice") Double maxPrice);
+
+    @Query(
+            "SELECT DISTINCT new com.example.aino_1.dto.SanPhamChiTietDto(" +
+                    "spct.id, spct.soLuong, sp.tenSanPham, cl.tenChatLieu, sp.gioiThieu, " +
+                    "ram.dungLuong, olt.dungLuong, mh.doPhanGiai, ktlt.kichThuoc, " +
+                    "mh.tamNen, mh.tanSoQuet, cpu.soNhan, gpu.kienTrucCongNghe, " +
+                    "cpu.ten, spct.maSpct, " +
+                    "spct.hinhAnhMinhHoa, " +
+                    "spct.donGia, sp.id, gpu.ten, sp.trongLuong" +
+                    ") " +
+                    "FROM SanPhamChiTiet spct " +
+                    "JOIN SanPham sp ON spct.sanPham.id = sp.id " +
+                    "INNER JOIN ChatLieu cl ON cl.id = sp.chatLieu.id " +
+                    "INNER JOIN LoaiSanPham lsp ON lsp.id = sp.loaiSanPham.id " +
+                    "INNER JOIN KichThuocLapTop ktlt ON ktlt.id = sp.kichThuocLaptop.id " +
+                    "INNER JOIN NguonNhap nn ON nn.id = sp.nguonNhap.id " +
+                    "INNER JOIN ManHinh mh ON mh.id = spct.manHinh.id " +
+                    "INNER JOIN Ram ram ON ram.id = spct.ram.id " +
+                    "INNER JOIN OLuuTru olt ON olt.id = spct.oLuuTru.id " +
+                    "INNER JOIN Cpu cpu ON cpu.id = spct.cpu.id " +
+                    "INNER JOIN Gpu gpu ON gpu.id = spct.gpu.id " +
+                    "WHERE ram.dungLuong = :dungLuongRam "
+    )
+    List<SanPhamChiTietDto> locTheoDungLuongRam(@Param("dungLuongRam") Integer dungLuongRam);
+
+    @Query(
+            "SELECT DISTINCT new com.example.aino_1.dto.SanPhamChiTietDto(" +
+                    "spct.id, spct.soLuong, sp.tenSanPham, cl.tenChatLieu, sp.gioiThieu, " +
+                    "ram.dungLuong, olt.dungLuong, mh.doPhanGiai, ktlt.kichThuoc, " +
+                    "mh.tamNen, mh.tanSoQuet, cpu.soNhan, gpu.kienTrucCongNghe, " +
+                    "cpu.ten, spct.maSpct, " +
+                    "spct.hinhAnhMinhHoa, " +
+                    "spct.donGia, sp.id, gpu.ten, sp.trongLuong" +
+                    ") " +
+                    "FROM SanPhamChiTiet spct " +
+                    "JOIN SanPham sp ON spct.sanPham.id = sp.id " +
+                    "INNER JOIN ChatLieu cl ON cl.id = sp.chatLieu.id " +
+                    "INNER JOIN LoaiSanPham lsp ON lsp.id = sp.loaiSanPham.id " +
+                    "INNER JOIN KichThuocLapTop ktlt ON ktlt.id = sp.kichThuocLaptop.id " +
+                    "INNER JOIN NguonNhap nn ON nn.id = sp.nguonNhap.id " +
+                    "INNER JOIN ManHinh mh ON mh.id = spct.manHinh.id " +
+                    "INNER JOIN Ram ram ON ram.id = spct.ram.id " +
+                    "INNER JOIN OLuuTru olt ON olt.id = spct.oLuuTru.id " +
+                    "INNER JOIN Cpu cpu ON cpu.id = spct.cpu.id " +
+                    "INNER JOIN Gpu gpu ON gpu.id = spct.gpu.id " +
+                    "WHERE spct.sanPham.nguonNhap.tenNhaCungUng = :hangSanXuat"
+    )
+    List<SanPhamChiTietDto> locTheoHangSanXuat(@Param("hangSanXuat") String hangSanXuat);
+
+    @Query(
+            "SELECT DISTINCT new com.example.aino_1.dto.SanPhamChiTietDto(" +
+                    "spct.id, spct.soLuong, sp.tenSanPham, cl.tenChatLieu, sp.gioiThieu, " +
+                    "ram.dungLuong, olt.dungLuong, mh.doPhanGiai, ktlt.kichThuoc, " +
+                    "mh.tamNen, mh.tanSoQuet, cpu.soNhan, gpu.kienTrucCongNghe, " +
+                    "cpu.ten, spct.maSpct, " +
+                    "spct.hinhAnhMinhHoa, " +
+                    "spct.donGia, sp.id, gpu.ten, sp.trongLuong" +
+                    ") " +
+                    "FROM SanPhamChiTiet spct " +
+                    "JOIN SanPham sp ON spct.sanPham.id = sp.id " +
+                    "INNER JOIN ChatLieu cl ON cl.id = sp.chatLieu.id " +
+                    "INNER JOIN LoaiSanPham lsp ON lsp.id = sp.loaiSanPham.id " +
+                    "INNER JOIN KichThuocLapTop ktlt ON ktlt.id = sp.kichThuocLaptop.id " +
+                    "INNER JOIN NguonNhap nn ON nn.id = sp.nguonNhap.id " +
+                    "INNER JOIN ManHinh mh ON mh.id = spct.manHinh.id " +
+                    "INNER JOIN Ram ram ON ram.id = spct.ram.id " +
+                    "INNER JOIN OLuuTru olt ON olt.id = spct.oLuuTru.id " +
+                    "INNER JOIN Cpu cpu ON cpu.id = spct.cpu.id " +
+                    "INNER JOIN Gpu gpu ON gpu.id = spct.gpu.id " +
+                    "WHERE mh.tamNen = :tamNen"
+    )
+    List<SanPhamChiTietDto> locTheoTamNen(@Param("tamNen") String tamNen);
+    @Query(
+            "SELECT DISTINCT new com.example.aino_1.dto.SanPhamChiTietDto(" +
+                    "spct.id, spct.soLuong, sp.tenSanPham, cl.tenChatLieu, sp.gioiThieu, " +
+                    "ram.dungLuong, olt.dungLuong, mh.doPhanGiai, ktlt.kichThuoc, " +
+                    "mh.tamNen, mh.tanSoQuet, cpu.soNhan, gpu.kienTrucCongNghe, " +
+                    "cpu.ten, spct.maSpct, " +
+                    "spct.hinhAnhMinhHoa, " +
+                    "spct.donGia, sp.id, gpu.ten, sp.trongLuong" +
+                    ") " +
+                    "FROM SanPhamChiTiet spct " +
+                    "JOIN SanPham sp ON spct.sanPham.id = sp.id " +
+                    "INNER JOIN ChatLieu cl ON cl.id = sp.chatLieu.id " +
+                    "INNER JOIN LoaiSanPham lsp ON lsp.id = sp.loaiSanPham.id " +
+                    "INNER JOIN KichThuocLapTop ktlt ON ktlt.id = sp.kichThuocLaptop.id " +
+                    "INNER JOIN NguonNhap nn ON nn.id = sp.nguonNhap.id " +
+                    "INNER JOIN ManHinh mh ON mh.id = spct.manHinh.id " +
+                    "INNER JOIN Ram ram ON ram.id = spct.ram.id " +
+                    "INNER JOIN OLuuTru olt ON olt.id = spct.oLuuTru.id " +
+                    "INNER JOIN Cpu cpu ON cpu.id = spct.cpu.id " +
+                    "INNER JOIN Gpu gpu ON gpu.id = spct.gpu.id " +
+                    "WHERE cpu.kienTrucCongNghe = :congNgheCPU"
+    )
+    List<SanPhamChiTietDto> locTheoCongNgheCPU(@Param("congNgheCPU") String congNgheCPU);
+    @Query(
+            "SELECT DISTINCT new com.example.aino_1.dto.SanPhamChiTietDto(" +
+                    "spct.id, spct.soLuong, sp.tenSanPham, cl.tenChatLieu, sp.gioiThieu, " +
+                    "ram.dungLuong, olt.dungLuong, mh.doPhanGiai, ktlt.kichThuoc, " +
+                    "mh.tamNen, mh.tanSoQuet, cpu.soNhan, gpu.kienTrucCongNghe, " +
+                    "cpu.ten, spct.maSpct, " +
+                    "spct.hinhAnhMinhHoa, " +
+                    "spct.donGia, sp.id, gpu.ten, sp.trongLuong" +
+                    ") " +
+                    "FROM SanPhamChiTiet spct " +
+                    "JOIN SanPham sp ON spct.sanPham.id = sp.id " +
+                    "INNER JOIN ChatLieu cl ON cl.id = sp.chatLieu.id " +
+                    "INNER JOIN LoaiSanPham lsp ON lsp.id = sp.loaiSanPham.id " +
+                    "INNER JOIN KichThuocLapTop ktlt ON ktlt.id = sp.kichThuocLaptop.id " +
+                    "INNER JOIN NguonNhap nn ON nn.id = sp.nguonNhap.id " +
+                    "INNER JOIN ManHinh mh ON mh.id = spct.manHinh.id " +
+                    "INNER JOIN Ram ram ON ram.id = spct.ram.id " +
+                    "INNER JOIN OLuuTru olt ON olt.id = spct.oLuuTru.id " +
+                    "INNER JOIN Cpu cpu ON cpu.id = spct.cpu.id " +
+                    "INNER JOIN Gpu gpu ON gpu.id = spct.gpu.id " +
+                    "WHERE ktlt.kichThuoc = :kichThuoc"
+    )
+    List<SanPhamChiTietDto> locTheoKichThuoc(@Param("kichThuoc") Double kichThuoc);
 }
-
-
-//    @Query("""
-//        SELECT new com.example.aino_1.dto.SanPhamChiTietDto(
-//        spct.id,
-//        spct.donGia,
-//        spct.tenSpct,
-//        spct.sanPham.hangSanXuat,
-//        spct.sanPham.mauSac,
-//        spct.sanPham.gioiThieu,
-//        spct.ram.dungLuong,
-//        spct.oLuuTru.dungLuong,
-//        spct.manHinh.doPhanGiai,
-//        spct.manHinh.kichThuoc,
-//        spct.manHinh.tamNen,
-//        spct.manHinh.tanSoQuet,
-//        spct.cpu.soNhan,
-//        spct.cpu.kienTrucCongNghe,
-//        spct.cpu.ten
-//        ) FROM SanPhamChiTiet spct
-//       WHERE spct.tenSpct LIKE %:tuKhoaTimKiem%
-//                   OR spct.cpu.ten LIKE %:tuKhoaTimKiem%
-//                   OR spct.manHinh.tamNen LIKE %:tuKhoaTimKiem%
-//                   OR CAST(spct.ram.dungLuong AS string) LIKE %:tuKhoaTimKiem%
-//                   OR spct.manHinh.doPhanGiai LIKE %:tuKhoaTimKiem%
-//                   OR CAST(spct.manHinh.tanSoQuet AS string) LIKE %:tuKhoaTimKiem%
-//                   OR CAST(spct.cpu.boNhoDem AS string) LIKE %:tuKhoaTimKiem%
-//                   OR spct.sanPham.gioiThieu LIKE %:tuKhoaTimKiem%
-//
-//""")
-//    List<SanPhamChiTietDto> timSanPhamTheoTuKhoa(@Param("tuKhoaTimKiem") String tuKhoaTimKiem);
-//    @Query("""
-//        SELECT new com.example.aino_1.dto.SanPhamChiTietDto(
-//        spct.id,
-//        spct.donGia,
-//        spct.tenSpct,
-//        spct.sanPham.hangSanXuat,
-//        spct.sanPham.gioiThieu,
-//        spct.ram.dungLuong,
-//        spct.oLuuTru.dungLuong,
-//        spct.manHinh.doPhanGiai,
-//        spct.manHinh.kichThuoc,
-//        spct.manHinh.tamNen,
-//        spct.manHinh.tanSoQuet,
-//        spct.cpu.soNhan,
-//        spct.cpu.kienTrucCongNghe,
-//        spct.cpu.ten
-//        ) FROM SanPhamChiTiet spct
-//       WHERE spct.donGia between :minPrice and :maxPrice
-//
-//""")
-//    List<SanPhamChiTietDto> locTheoGia(@Param("minPrice") Double minPrice, @Param("maxPrice") Double maxPrice);
-//    @Query("""
-//        SELECT new com.example.aino_1.dto.SanPhamChiTietDto(
-//        spct.id,
-//        spct.donGia,
-//        spct.tenSpct,
-//        spct.sanPham.hangSanXuat,
-//        spct.sanPham.mauSac,
-//        spct.sanPham.gioiThieu,
-//        spct.ram.dungLuong,
-//        spct.oLuuTru.dungLuong,
-//        spct.manHinh.doPhanGiai,
-//        spct.manHinh.kichThuoc,
-//        spct.manHinh.tamNen,
-//        spct.manHinh.tanSoQuet,
-//        spct.cpu.soNhan,
-//        spct.cpu.kienTrucCongNghe,
-//        spct.cpu.ten
-//        ) FROM SanPhamChiTiet spct
-//       WHERE spct.ram.dungLuong = :dungLuongRam
-//
-//""")
-//    List<SanPhamChiTietDto> locTheoDungLuongRam(@Param("dungLuongRam") Integer dungLuongRam);
-//    @Query("""
-//        SELECT new com.example.aino_1.dto.SanPhamChiTietDto(
-//        spct.id,
-//        spct.donGia,
-//        spct.tenSpct,
-//        spct.sanPham.hangSanXuat,
-//        spct.sanPham.mauSac,
-//        spct.sanPham.gioiThieu,
-//        spct.ram.dungLuong,
-//        spct.oLuuTru.dungLuong,
-//        spct.manHinh.doPhanGiai,
-//        spct.manHinh.kichThuoc,
-//        spct.manHinh.tamNen,
-//        spct.manHinh.tanSoQuet,
-//        spct.cpu.soNhan,
-//        spct.cpu.kienTrucCongNghe,
-//        spct.cpu.ten
-//        ) FROM SanPhamChiTiet spct
-//       WHERE spct.sanPham.hangSanXuat = :hangSanXuat
-//
-//""")
-//    List<SanPhamChiTietDto> locTheoHangSanXuat(@Param("hangSanXuat") String hangSanXuat);
-//    @Query("""
-//        SELECT new com.example.aino_1.dto.SanPhamChiTietDto(
-//        spct.id,
-//        spct.donGia,
-//        spct.tenSpct,
-//        spct.sanPham.hangSanXuat,
-//        spct.sanPham.mauSac,
-//        spct.sanPham.gioiThieu,
-//        spct.ram.dungLuong,
-//        spct.oLuuTru.dungLuong,
-//        spct.manHinh.doPhanGiai,
-//        spct.manHinh.kichThuoc,
-//        spct.manHinh.tamNen,
-//        spct.manHinh.tanSoQuet,
-//        spct.cpu.soNhan,
-//        spct.cpu.kienTrucCongNghe,
-//        spct.cpu.ten
-//        ) FROM SanPhamChiTiet spct
-//       WHERE spct.manHinh.tamNen = :tamNen
-//
-//""")
-//    List<SanPhamChiTietDto> locTheoTamNen(@Param("tamNen") String tamNen);
-//    @Query("""
-//        SELECT new com.example.aino_1.dto.SanPhamChiTietDto(
-//        spct.id,
-//        spct.donGia,
-//        spct.tenSpct,
-//        spct.sanPham.hangSanXuat,
-//        spct.sanPham.mauSac,
-//        spct.sanPham.gioiThieu,
-//        spct.ram.dungLuong,
-//        spct.oLuuTru.dungLuong,
-//        spct.manHinh.doPhanGiai,
-//        spct.manHinh.kichThuoc,
-//        spct.manHinh.tamNen,
-//        spct.manHinh.tanSoQuet,
-//        spct.cpu.soNhan,
-//        spct.cpu.kienTrucCongNghe,
-//        spct.cpu.ten
-//        ) FROM SanPhamChiTiet spct
-//       WHERE spct.cpu.kienTrucCongNghe = :congNgheCPU
-//
-//""")
-//    List<SanPhamChiTietDto> locTheoCongNgheCPU(@Param("congNgheCPU") String congNgheCPU);
-//    @Query("""
-//        SELECT new com.example.aino_1.dto.SanPhamChiTietDto(
-//        spct.id,
-//        spct.donGia,
-//        spct.tenSpct,
-//        spct.sanPham.hangSanXuat,
-//        spct.sanPham.mauSac,
-//        spct.sanPham.gioiThieu,
-//        spct.ram.dungLuong,
-//        spct.oLuuTru.dungLuong,
-//        spct.manHinh.doPhanGiai,
-//        spct.manHinh.kichThuoc,
-//        spct.manHinh.tamNen,
-//        spct.manHinh.tanSoQuet,
-//        spct.cpu.soNhan,
-//        spct.cpu.kienTrucCongNghe,
-//        spct.cpu.ten
-//        ) FROM SanPhamChiTiet spct
-//       WHERE spct.manHinh.kichThuoc = :kichThuoc
-//
-//""")
-//    List<SanPhamChiTietDto> locTheoKichThuoc(@Param("kichThuoc") Double kichThuoc);
-//}

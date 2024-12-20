369a92f1b5030571b32b6bb2a2a7672be7a908d2
namtt <namtt2304@gmail.com>
2024-12-20 22:02:39 +0700

    fixAddSPCT

--- src/main/java/com/example/aino_1/Aino1Application.java
+++ src/main/java/com/example/aino_1/Aino1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Aino1Application {
+public class    Aino1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Aino1Application.class, args);

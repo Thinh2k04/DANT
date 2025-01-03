9b950a8e60000002a63e618c87990a29d8a49515
Tsan333 <149345264+Tsan333@users.noreply.github.com>
2025-01-03 18:14:13 +0700

    updateThongKe

--- src/main/java/com/example/aino_1/repository/ThongKeInterface.java
+++ src/main/java/com/example/aino_1/repository/ThongKeInterface.java
@@ -1,5 +1,6 @@
 package com.example.aino_1.repository;
 
+import com.example.aino_1.dto.ThongKeDTO;
 import com.example.aino_1.entity.GioHang;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,43 +11,74 @@ import java.util.List;
 
 @Repository
 public interface ThongKeInterface extends JpaRepository<GioHang, Integer> {
+    // Thống kê doanh thu theo ngày hôm nay
+    @Query("""
+    SELECT new com.example.aino_1.dto.ThongKeDTO(
+        FORMAT(hd.thoiGianLapHoaDon, 'yyyy-MM-dd'), 
+        SUM(hd.tongTien)) 
+    FROM HoaDon hd 
+    WHERE hd.trangThaiThanhToan = 1 
+    AND FORMAT(hd.thoiGianLapHoaDon, 'yyyy-MM-dd') = :ngay 
+    GROUP BY FORMAT(hd.thoiGianLapHoaDon, 'yyyy-MM-dd')                                                                 
+""")
+    List<ThongKeDTO> findTongTienTheoNgay(@Param("ngay") String ngay);
+
+    // Thống kê doanh thu theo từng ngày
+    @Query("""
+            SELECT new com.example.aino_1.dto.ThongKeDTO(
+            FORMAT(hd.thoiGianLapHoaDon, 'yyyy-MM-dd'), 
+            SUM(hd.tongTien)) 
+            FROM HoaDon hd
+            WHERE hd.trangThaiThanhToan = 1
+            GROUP BY FORMAT(hd.thoiGianLapHoaDon, 'yyyy-MM-dd')
+            ORDER BY FORMAT(hd.thoiGianLapHoaDon, 'yyyy-MM-dd')
+            """)
+    List<ThongKeDTO> getDailyRevenue();
     // Thống kê doanh thu theo tháng
-    @Query(value = """
-            SELECT FORMAT(hd.thoiGianLapHoaDon, 'yyyy-MM') , SUM(hd.tongTien) 
+    @Query( """
+            SELECT new com.example.aino_1.dto.ThongKeDTO(
+            FORMAT(hd.thoiGianLapHoaDon, 'yyyy-MM'), 
+            SUM(hd.tongTien)) 
             FROM HoaDon hd
             WHERE hd.trangThaiThanhToan = 1
             GROUP BY FORMAT(hd.thoiGianLapHoaDon, 'yyyy-MM')
             ORDER BY FORMAT(hd.thoiGianLapHoaDon, 'yyyy-MM')
             """)
-    List<Object[]> getMonthlyRevenue();
+    List<ThongKeDTO> getMonthlyRevenue();
     // Thống kê doanh thu theo từng tháng
-    @Query(value = """
-            SELECT FORMAT(hd.thoiGianLapHoaDon, 'yyyy-MM') , SUM(hd.tongTien) 
+    @Query( """
+            SELECT new com.example.aino_1.dto.ThongKeDTO(
+            FORMAT(hd.thoiGianLapHoaDon, 'yyyy-MM'), 
+            SUM(hd.tongTien)) 
             FROM HoaDon hd
             WHERE hd.trangThaiThanhToan = 1 AND MONTH(hd.thoiGianLapHoaDon) = :thang
             GROUP BY FORMAT(hd.thoiGianLapHoaDon, 'yyyy-MM')
             ORDER BY FORMAT(hd.thoiGianLapHoaDon, 'yyyy-MM')
             """)
-    Object[] getMonth(@Param("thang") Integer thang);
+    ThongKeDTO getMonth(@Param("thang") Integer thang);
 
     @Query("""
-            SELECT YEAR(hd.thoiGianLapHoaDon), SUM(hd.tongTien)
+            SELECT new com.example.aino_1.dto.ThongKeDTO(
+            YEAR(hd.thoiGianLapHoaDon), 
+            SUM(hd.tongTien)) 
             FROM HoaDon hd
             WHERE hd.trangThaiThanhToan = 1 AND YEAR(hd.thoiGianLapHoaDon) = :nam
             GROUP BY YEAR(hd.thoiGianLapHoaDon)
             ORDER BY YEAR(hd.thoiGianLapHoaDon)
             """)
-    Object[] getYear(@Param("nam") Integer nam);
+    ThongKeDTO getYear(@Param("nam") Integer nam);
 
     //  Thống kê doanh thu theo năm
     @Query("""
-            SELECT YEAR(hd.thoiGianLapHoaDon), SUM(hd.tongTien)
+            SELECT new com.example.aino_1.dto.ThongKeDTO(
+            YEAR(hd.thoiGianLapHoaDon), 
+            SUM(hd.tongTien)) 
             FROM HoaDon hd
             WHERE hd.trangThaiThanhToan = 1
             GROUP BY YEAR(hd.thoiGianLapHoaDon)
             ORDER BY YEAR(hd.thoiGianLapHoaDon)
             """)
-    List<Object[]> getYearlyRevenue();
+    List<ThongKeDTO> getYearlyRevenue();
 
     // đếm số đơn thanh toán
     @Query("""

21722804aa55a7b4bd2d4c41d212e3a6344c9c71
namtt <namtt2304@gmail.com>
2025-01-15 00:39:16 +0700

    update

--- src/main/java/com/example/aino_1/restController/ThuongHieuRestController.java
+++ src/main/java/com/example/aino_1/restController/ThuongHieuRestController.java
@@ -29,7 +29,7 @@ public class ThuongHieuRestController {
         return thsi.findById(id).get();
     }
 
-    @PostMapping("/update/{id}")
+    @PostMapping("/update")
     public ThuongHieu update(@RequestBody ThuongHieu tk) {
         return thsi.save(tk);
     }
